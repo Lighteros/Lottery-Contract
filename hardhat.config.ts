cde3ff69eeaa32fe9b739e46f681739a7829c787
dskydiver <dastanmukhan6@gmail.com>
2023-11-23 10:06:09 -0500

    update hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,11 +1,19 @@
 import { HardhatUserConfig } from 'hardhat/config'
 import '@nomicfoundation/hardhat-toolbox-viem'
+const { vars } = require('hardhat/config')
+
+const MAINNET_URL = vars.get('MAINNET_URL')
+const PRIVATE_KEY = vars.get('PRIVATE_KEY')
 
 const config: HardhatUserConfig = {
   solidity: '0.8.20',
   networks: {
     localhost: {
       url: 'http://127.0.0.1:8545',
+    },
+    ethereum: {
+      url: MAINNET_URL,
+      accounts: [PRIVATE_KEY],
     },
   },
 }
