a01f747580540d6c8335c1c19714d480b045dcc7
dskydiver <dastanmukhan6@gmail.com>
2023-11-28 03:05:43 -0500

    update config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,8 +2,11 @@ import { HardhatUserConfig } from 'hardhat/config'
 import '@nomicfoundation/hardhat-toolbox-viem'
 import { vars } from 'hardhat/config'
 
-// const MAINNET_URL = vars.get('MAINNET_URL')
-// const PRIVATE_KEY = vars.get('PRIVATE_KEY')
+const MAINNET_URL = vars.get('MAINNET_URL', 'https://mainnet.infura.io/v3/')
+const PRIVATE_KEY = vars.get(
+  'PRIVATE_KEY',
+  '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80'
+)
 
 const config: HardhatUserConfig = {
   solidity: '0.8.20',
@@ -11,10 +14,13 @@ const config: HardhatUserConfig = {
     localhost: {
       url: 'http://127.0.0.1:8545',
     },
-    // ethereum: {
-    //   url: MAINNET_URL,
-    //   accounts: [PRIVATE_KEY],
-    // },
+    ethereum: {
+      url: MAINNET_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: vars.get('ETHERSCAN_API_KEY', ''),
   },
 }
 
